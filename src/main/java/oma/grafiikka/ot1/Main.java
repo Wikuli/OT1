4955f7726faccfac69d617d3d1675231e934b6c0
Wikuli <tiilikainen.santeri@gmail.com>
2024-04-09 20:47:59 +0300

    main add

--- src/main/java/oma/grafiikka/ot1/Main.java
+++ src/main/java/oma/grafiikka/ot1/Main.java
@@ -1,11 +1,12 @@
 package oma.grafiikka.ot1;
 
 import javafx.application.Application;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage){
-
+        Rectangle yesthisisarectangle = new Rectangle();
     }
 }

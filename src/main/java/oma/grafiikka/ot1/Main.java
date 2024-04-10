5106b0f3dbda5627fcca0cb001e68ff136f27a17
KuhaParki <niko.loytainen@elisanet.fi>
2024-04-10 16:25:02 +0300

    Luotiin testi-fxml-tiedosto ja lisättiin se resources kansioon

--- src/main/java/oma/grafiikka/ot1/Main.java
+++ src/main/java/oma/grafiikka/ot1/Main.java
@@ -1,20 +1,32 @@
 package oma.grafiikka.ot1;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
     @Override
-    public void start(Stage stage){
-        Rectangle yesthisisarectangle = new Rectangle();
+    public void start(Stage stage) throws Exception {
 
-        // Tein ympyrän, jota ei ole määritelty
-        Circle nothisisnotarectangle = new Circle();
-        Circle pylpyrä = new Circle();
+        Parent root = FXMLLoader.load(getClass().getResource("/Test.fxml"));
 
-        //Tämä on Diman suorakulmio
-        Rectangle dimaRectum = new Rectangle();
+        Scene scene = new Scene(root, 600, 400);
+
+        stage.setScene(scene);
+
+        stage.setTitle("FXML Example");
+
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }

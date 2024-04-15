79b140834dea4a7d1171c129cc9f1a5a22bad22c
dmytros <mr.strant@mail.ru>
2024-04-15 11:31:45 +0300

    Tein palvelu ja teen Varauksen_palvelut

--- src/main/java/oma/grafiikka/ot1/Palvelu.java
+++ src/main/java/oma/grafiikka/ot1/Palvelu.java
@@ -1,10 +1,25 @@
 package oma.grafiikka.ot1;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import javafx.scene.control.Alert;
 
 @Entity
 @Table(name = "palvelu")
 public class Palvelu {
-
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "palvelu_id")
+    private int palvelu_id;
+    @ManyToOne(fetch = FetchType.LAZY) //Vai pitääkö olla OneToMany?
+    @JoinColumn(name = "alue_id")
+    private Alue alue;    // vai pitääkö olla private int alue_id ?
+    @Column(name = "nimi")
+    private String nimi;
+    @Column(name = "kuvaus")
+    private String kuvaus;
+    @Column (name = "hinta")
+    private double hinta;
+    @Column (name = "alv")
+    private double alv;
 }
+
--- src/main/java/oma/grafiikka/ot1/Varauksen_palvelut.java
+++ src/main/java/oma/grafiikka/ot1/Varauksen_palvelut.java
@@ -1,4 +1,13 @@
 package oma.grafiikka.ot1;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "varauksen_palvelut" )
 public class Varauksen_palvelut {
+
+    private Varaus
 }

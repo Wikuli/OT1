51c54fc12965bb3c5f4231c23b33aa91c955d3b7
KuhaParki <niko.loytainen@elisanet.fi>
2024-04-15 19:16:37 +0300

    Tehtiin uita lisää ja lisäiltiin ikkunoita aloitusnäkymän painikkeille. Myös joitakin kommentteja lisätty. Vielä ikkunoista puuttuu Laskujen hallinta, Laskujen seuranta, Asiakashallinta ja Palveluiden hallinta.

--- src/main/java/oma/grafiikka/ot1/Controller.java
+++ src/main/java/oma/grafiikka/ot1/Controller.java
@@ -11,77 +11,35 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 /**
- * This class contains methods that give functionality to the elements in the ui.
+ * This class contains methods that give functionality to the elements in the ui. HUOM!!! Ensimmäiset 10 metodia ovat
+ * järjestyksessä Buttonit aloitusnäytöltä. Tämän seikan huomioiminen saattaa paranttaa luettavuutta. Muut metodit ovat
+ * Aukeavien ikkunoiden sisällä olevia metodeja. Metodeja niimmaan perkeleesti.
  */
 public class Controller {
 
     public TextField addArea;
 
     /**
-     * Method for the "Uusi varaus" button functionality. Opens a new window when clicked.
-     * @param actionEvent Equals a click of the button from the user
+     * Tällä metodilla voidaan avata uusi ikkunta "Poista varaus" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
      */
-    public void newReservation(ActionEvent actionEvent) throws IOException {
+    public void deleteReservation(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/uusiVaraus.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/poistaVaraus.fxml"));
 
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
-        stage.setTitle("FXML Example");
+        stage.setTitle("Poista varaus");
         stage.show();
-
     }
 
     /**
-     * Method for the "Asiakkaiden hallinta" button functionality
-     * @param actionEvent Equals a click of the button from the user
+     * Tällä metodilla voidaan avata uusi ikkuna "Etsi mökkejä" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
      */
-    public void manageCustomers(ActionEvent actionEvent) {
-        System.out.println("Asiakkaita hallittu");
-    }
-
-    /**
-     * Method for the "Laskutus" button functionality
-     * @param actionEvent Equals a click of the button from the user
-     */
-    public void manageInvoice(ActionEvent actionEvent) {
-        System.out.println("Laskuja hallittu");
-    }
-
-    /**
-     * Method for the "Raportit" button functionality
-     * @param actionEvent Equals a click of the button from the user
-     */
-    public void seeReports(ActionEvent actionEvent) {
-        System.out.println("Raportteja katseltu");
-    }
-
-    /**
-     * Method for the "Alueiden hallinta" button functionality
-     * @param actionEvent Equals a click of the button from the user
-     */
-    public void manageAreas(ActionEvent actionEvent) {
-        System.out.println("Alueita hallittu");
-    }
-
-    /**
-     * Method for the "Palveluiden hallinta" button functionality
-     * @param actionEvent Equals a click of the button from the user
-     */
-    public void manageServices(ActionEvent actionEvent) {
-        System.out.println("Palveluita hallittu");
-    }
-
-    /**
-     * Metodi, jolla voi lisätä uuden alueen
-     * @param actionEvent
-     */
-    public void addNewArea(ActionEvent actionEvent) {
-        Alue alue = new Alue(addArea.getText());
-        alue.lisaaAlue(alue);
-    }
-
     public void findCabins(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/etsiMokkeja.fxml"));
@@ -93,12 +51,14 @@ public class Controller {
         stage.show();
     }
 
-    public void trackInvoice(ActionEvent actionEvent) {
-    }
-
-    public void deleteReservation(ActionEvent actionEvent) throws IOException {
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Uusi varaus" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
+    public void newReservation(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("/poistaVaraus.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/uusiVaraus.fxml"));
 
         Scene scene = new Scene(root);
 
@@ -107,6 +67,28 @@ public class Controller {
         stage.show();
     }
 
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Laskujen hallinta" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
+    public void manageInvoice(ActionEvent actionEvent) throws IOException{
+        System.out.println("Laskuja hallittu");
+    }
+
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Laskujen seuranta" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
+    public void trackInvoice(ActionEvent actionEvent) throws IOException{
+    }
+
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Majoitusten raportit" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
     public void seeAccommodationReports(ActionEvent actionEvent) throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/majoitustenRaportit.fxml"));
@@ -118,6 +100,92 @@ public class Controller {
         stage.show();
     }
 
-    public void seeServiceReports(ActionEvent actionEvent) {
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Palveluiden raportit" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
+    public void seeServiceReports(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/palveluidenRaportit.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("FXML Example");
+        stage.show();
+    }
+
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Alueiden hallinta" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
+    public void manageAreas(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/alueidenHallinta.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("FXML Example");
+        stage.show();
+    }
+
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Asiakashallinta" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
+    public void manageCustomers(ActionEvent actionEvent) throws IOException {
+        System.out.println("Asiakkaita hallittu");
+    }
+
+
+    /**
+     * Tällä metodilla voidaan avata uusi ikkuna "Asiakashallinta" nappia painamalla
+     * @param actionEvent Napin painallus
+     * @throws IOException Heitetään exception, jos haluttua fxml-tiedostoa ei ole.
+     */
+    public void manageServices(ActionEvent actionEvent) throws IOException {
+    }
+
+    /**
+     * Metodi, jolla voi lisätä uuden alueen
+     * @param actionEvent
+     */
+    public void addNewArea(ActionEvent actionEvent) {
+        Alue alue = new Alue(addArea.getText());
+        alue.lisaaAlue(alue);
+    }
+
+    public void deleteArea(ActionEvent actionEvent) {
+    }
+
+    public void addArea(ActionEvent actionEvent) {
+    }
+
+    public void deleteCustomer(ActionEvent actionEvent) {
+    }
+
+    public void changeCustomerInformation(ActionEvent actionEvent) {
+    }
+
+    public void checkReportOnAccommodation(ActionEvent actionEvent) {
+    }
+
+    public void findReservations(ActionEvent actionEvent) {
+    }
+
+    public void deleteThisReservation(ActionEvent actionEvent) {
+    }
+
+    public void findThisCabin(ActionEvent actionEvent) {
+    }
+
+    public void makeReservation(ActionEvent actionEvent) {
+    }
+
+    public void serviceReportSearch(ActionEvent actionEvent) {
     }
 }

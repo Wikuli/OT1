6b7d45bca0b2feb43aef85fc13b3aa5f4eab8953
Wikuli <tiilikainen.santeri@gmail.com>
2024-04-15 10:59:40 +0300

    ORM

--- src/main/java/oma/grafiikka/ot1/Palvelu.java
+++ src/main/java/oma/grafiikka/ot1/Palvelu.java
@@ -1,4 +1,10 @@
 package oma.grafiikka.ot1;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "palvelu")
 public class Palvelu {
+
 }
--- src/main/java/oma/grafiikka/ot1/Posti.java
+++ src/main/java/oma/grafiikka/ot1/Posti.java
@@ -1,4 +1,42 @@
 package oma.grafiikka.ot1;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "posti")
 public class Posti {
+    @Id
+    @Column(name = "postinro")
+    private String posti;
+    @Column(name = "toimipaikka")
+    private String toimipaikka;
+
+    @OneToMany(mappedBy = "posti")
+    private List<Asiakas> asiakkaat;
+
+    @OneToMany(mappedBy = "posti")
+    private List<Mokki> mokit;
+
+    public Posti(String posti, String toimipaikka) {
+        this.posti = posti;
+        this.toimipaikka = toimipaikka;
+    }
+
+    public String getPosti() {
+        return posti;
+    }
+
+    public void setPosti(String posti) {
+        this.posti = posti;
+    }
+
+    public String getToimipaikka() {
+        return toimipaikka;
+    }
+
+    public void setToimipaikka(String toimipaikka) {
+        this.toimipaikka = toimipaikka;
+    }
 }

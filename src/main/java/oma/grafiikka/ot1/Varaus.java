6482392f423c27bfea6b91c5df7755f9de84320d
Jorkula <arttu.vitri@gmail.com>
2024-04-24 08:04:00 +0300

    Lisäsin javadoc kommentteja

--- src/main/java/oma/grafiikka/ot1/Varaus.java
+++ src/main/java/oma/grafiikka/ot1/Varaus.java
@@ -16,27 +16,57 @@ import java.util.List;
 @Entity
 @Table(name = "varaus")
 public class Varaus {
+    /**
+     *kenttä varauksen id:lle
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "varaus_id")
     private int varaus_id;
+    /**
+     * kenttä asiakas oliolle
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "asiakas_id")
     private Asiakas asiakas;
+    /**
+     * Kenttä mökki oliolle
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "mokki_id")
     private Mokki mokki;
+    /**
+     * kenttä varatuille päiville
+     */
     @Column(name = "varattu_pvm", columnDefinition = "DATETIME")
     private Date varattu_pvm;
+    /**
+     *kenttä vahvistus päivälle
+     */
     @Column(name = "vahvistus_pvm", columnDefinition = "DATETIME")
     private Date vahvistus_pvm;
+    /**
+     * kenttä varauksen aloituspäivälle
+     */
     @Column(name = "varattu_alkupvm", columnDefinition = "DATETIME")
     private Date varattu_alkupvm;
+    /**
+     * kenttä varauksen loppumis päivälle
+     */
     @Column(name = "varattu_loppupvm", columnDefinition = "DATETIME")
     private Date varattu_loppupvm;
     //@OneToMany (mappedBy = "varaus")
     //private List<Varauksen_palvelut> varauksenPalveluList;
 
+    /**
+     * varausten alustaja
+     * @param asiakas eli asiakas olio
+     * @param mokki eli mökki olio
+     * @param varattu_pvm eli mökin päivät jolloin jo varattu
+     * @param vahvistus_pvm eli varauksen vahvistus päivä
+     * @param varattu_alkupvm varauksen aloitus päivä
+     * @param varattu_loppupvm varauksen loppu päivä
+     */
     public Varaus(Asiakas asiakas, Mokki mokki, Date varattu_pvm, Date vahvistus_pvm, Date varattu_alkupvm, Date varattu_loppupvm) {
         this.asiakas = asiakas;
         this.mokki = mokki;
@@ -50,50 +80,98 @@ public class Varaus {
 
     }
 
+    /**
+     * Get-metodi varaus_id:lle
+     * @return varaus_id eli varauksen id
+     */
     public int getVaraus_id() {
         return varaus_id;
     }
 
+    /**
+     * get-metodi asiakkaalle
+     * @return asiakas eli asiakas olio
+     */
     public Asiakas getAsiakas() {
         return asiakas;
     }
 
+    /**
+     * Set-metodi asiakkaalle
+     * @param asiakas eli asiakas olio
+     */
     public void setAsiakas(Asiakas asiakas) {
         this.asiakas = asiakas;
     }
 
+    /**
+     * Get-metodi mökille
+     * @return mokki eli mökki lio
+     */
     public Mokki getMokki() {
         return mokki;
     }
 
+    /**
+     * set-metodi mökille
+     * @param mokki eli mökki olio
+     */
     public void setMokki(Mokki mokki) {
         this.mokki = mokki;
     }
 
+    /**
+     * Get-metodi varatuille päiville
+     * @return varattu_pvm eli varatut päivät
+     */
     public Date getVarattu_pvm() {
         return varattu_pvm;
     }
 
+    /**
+     * Set-metodi varatuille päiville
+     * @param varattu_pvm eli varatut päivät
+     */
     public void setVarattu_pvm(Date varattu_pvm) {
         this.varattu_pvm = varattu_pvm;
     }
 
+    /**
+     * Get-metodi vahvistus päivämäärälle
+     * @return vahvistus_pvm eli vahvistus päiivämäärä
+     */
     public Date getVahvistus_pvm() {
         return vahvistus_pvm;
     }
 
+    /**
+     * Set-metodi vahvistus päivämäärälle
+     * @param vahvistus_pvm eli vahvistus päivämäärä
+     */
     public void setVahvistus_pvm(Date vahvistus_pvm) {
         this.vahvistus_pvm = vahvistus_pvm;
     }
 
+    /**
+     * Get-metodi varauksen alkupäivämäärälle
+     * @return varattu_alkupvm eli varauksen aloitus pvm
+     */
     public Date getVarattu_alkupvm() {
         return varattu_alkupvm;
     }
 
+    /**
+     * Set-metodi varauksen alkupäivämäärälle
+     * @param varattu_alkupvm eli varauksen aloitus päivä
+     */
     public void setVarattu_alkupvm(Date varattu_alkupvm) {
         this.varattu_alkupvm = varattu_alkupvm;
     }
 
+    /**
+     * Get-metodi varauksen loppupäivämäärälle
+     * @return varattu_loppupvm
+     */
     public Date getVarattu_loppupvm() {
         return varattu_loppupvm;
     }

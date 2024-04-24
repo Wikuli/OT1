80e1bcfbccfc4fa0e0cec93dfa245c78e0a7cc8c
Jorkula <arttu.vitri@gmail.com>
2024-04-24 08:18:55 +0300

    Lisäsin javadoc kommentteja. Lisäksi muokkasin paria metodia

--- src/main/java/oma/grafiikka/ot1/Asiakas.java
+++ src/main/java/oma/grafiikka/ot1/Asiakas.java
@@ -15,26 +15,56 @@ import java.util.List;
 @Entity
 @Table (name = "asiakas")
 public class Asiakas {
+    /**
+     * kenttä asiakkaan id:lle
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "asiakas_id")
     private int asiakas_id;
+    /**
+     * kenttä asiakkaan postinumerolle
+     */
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "postinro")
     private Posti posti;
+    /**
+     * Kenttä etunimelle
+     */
     @Column(name = "etunimi")
     private String etunimi;
+    /**
+     * Kenttä sukunimelle
+     */
     @Column(name = "sukunimi")
     private String sukunimi;
+    /**
+     * Kenttä kotiosoitteelle
+     */
     @Column(name = "lahiosoite")
     private String lahiosoite;
+    /**
+     * Kenttä sähköpostille
+     */
     @Column(name = "email")
     private String email;
+    /**
+     * Kenttä puhelinnumerolle
+     */
     @Column(name = "puhelinnro")
     private String puhelinnro;
     @OneToMany(mappedBy = "asiakas")
     private List<Varaus> varaukset;
 
+    /**
+     * alustaja asiakas oliolle
+     * @param posti eli postinumero
+     * @param etunimi eli etunimi
+     * @param sukunimi eli sukunimi
+     * @param lahiosoite eli kotiosoite
+     * @param email eli sähköposti
+     * @param puhelinnro eli puhelinnumero
+     */
     public Asiakas(Posti posti, String etunimi, String sukunimi, String lahiosoite, String email, String puhelinnro){
         this.posti = posti;
         this.etunimi = etunimi;
@@ -49,9 +79,15 @@ public class Asiakas {
     public void setPosti(Posti posti){
         this.posti = posti;
     }
+    //Tämä taitaa olla turha?
     public Posti getPosti(){
         return posti;
     }
+
+    /**
+     * Get-metodi asiakkaan postinumerolle
+     * @return nro eli asiakkaan postinumero
+     */
     public String getPostiNro(){
         try(Session session = Main.sessionFactory.openSession()){
             Transaction transaction = session.beginTransaction();
@@ -64,55 +100,128 @@ public class Asiakas {
         }
     }
 
+    /**
+     * Get-metodi asiakas id:lle
+     * @return asiakas_id eli asiakas id
+     */
     public int getAsiakas_id() {
         return asiakas_id;
     }
 
+    /**
+     * Set-metodi asiakas_id:lle
+     * @param asiakas_id eli asiakas id
+     */
     public void setAsiakas_id(int asiakas_id) {
         this.asiakas_id = asiakas_id;
     }
 
+    /**
+     * Get-metodi asiakkaan etunimelle
+     * @return etunimi eli asiakkaan etunimi
+     */
     public String getEtunimi() {
         return etunimi;
     }
 
+    /**
+     * Set-metodi asiakkaan etunimelle
+     * @param etunimi eli asiakkaan etunimi
+     */
     public void setEtunimi(String etunimi) {
         this.etunimi = etunimi;
     }
 
+    /**
+     * Get-metodi asiakkaan sukunimelle
+     * @return sukunimi eli asiakkaan sukunimi
+     */
     public String getSukunimi() {
         return sukunimi;
     }
 
+    /**
+     * Set-metodi asiakkaan sukunimelle
+     * @param sukunimi eli asiakkaan sukunimi
+     */
     public void setSukunimi(String sukunimi) {
         this.sukunimi = sukunimi;
     }
 
+    /**
+     * get-metodi asiakkaan lähiosoitteelle
+     * @return lahiosoite eli asiakkaan kotiosoite
+     */
     public String getLahiosoite() {
         return lahiosoite;
     }
 
+    /**
+     * Set-metodi asiakkaan lähiosoitteelle
+     * @param lahiosoite eli asiakkaan lähiosoite
+     */
     public void setLahiosoite(String lahiosoite) {
         this.lahiosoite = lahiosoite;
     }
 
+    /**
+     * Get-metodi asiakkaan sähköpostille
+     * @return email eli sähköposti
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Set-metodi asiakkaan sähköpostille
+     * @param email eli sähköposti
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Get-metodi asiakkaan puhelinnumerolle
+     * @return puhelinnumero
+     */
     public String getPuhelinnro() {
         return puhelinnro;
     }
 
+    /**
+     * Set-metodi asiakkaan puhelinnumerolle
+     * @param puhelinnro eli asiakkaan puhelinnumero
+     */
     public void setPuhelinnro(String puhelinnro) {
         this.puhelinnro = puhelinnro;
     }
 
+    /**
+     * metodi asiakkaan lisäämiselle
+     * @param asiakas eli asiakas olio
+     * @param sessionFactory
+     */
+    public static void lisaaAsiakas(Asiakas asiakas, SessionFactory sessionFactory){
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(asiakas);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * metodi asiakkaan poistamiselle
+     * @param asiakas eli asiakas olio
+     */
+    public static void poistaAsiakas(Asiakas asiakas){
+        try(Session session = Main.sessionFactory.openSession()){
+            Transaction transaction = session.beginTransaction();
+            session.remove(asiakas);
+            transaction.commit();
+        }
+    }
     /**
      * metodi jonka avulla päästään näkemään lista kaikista asiakkaista
      * @return listan asiakkaista
